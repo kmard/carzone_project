0ab73e76df7176be4f48e6890967be04e5e3aacc
KM <tsnok777@gmail.com>
2022-11-21 15:09:56 +0200

    Connect to page contact.html, services.html, about.html

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    # return HttpResponse('ok')
     return render(request, 'pages/home.html',{})
+def about(request):
+    return render(request, 'pages/about.html', {})
+def contact(request):
+    return render(request, 'pages/contact.html', {})
+def services(request):
+    return render(request, 'pages/services.html', {})
