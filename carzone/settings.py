0bd4c3399304d422171136719eb535e408ccbea2
KM <tsnok777@gmail.com>
2022-11-25 22:31:28 +0200

    Model Team

--- carzone/settings.py
+++ carzone/settings.py
@@ -126,6 +126,10 @@ os.path.join(BASE_DIR,'carzone/static'),
 ]
 #python manage.py collectstatic
 
+#Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
