c171660c11fdf8a01a8c2734863eb88d8a3e1899
KM <tsnok777@gmail.com>
2022-11-29 01:38:47 +0200

    Model Team

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html',{})
+    team = Team.objects.all()
+    data = {
+        'teams':team,
+    }
+    return render(request, 'pages/home.html',data)
 def about(request):
-    return render(request, 'pages/about.html', {})
+    team = Team.objects.all()
+    data  = {
+        'teams':team,
+    }
+    return render(request, 'pages/about.html', data)
 def contact(request):
     return render(request, 'pages/contact.html', {})
 def services(request):
