98275130eded6cafb0c4512680131a69b3303033
KM <tsnok777@gmail.com>
2022-12-04 13:26:53 +0200

    add app cars

--- cars/urls.py
+++ cars/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('cars/', include('cars.urls')),
-] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    path('', views.cars,name='cars'),
+    # path('about', views.about,name='about'),
+    # path('services', views.services,name='services'),
+    # path('contact', views.contact,name='contact'),
+
+]
