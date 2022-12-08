300853282ab870fff3adc3a81068c357647c479c
KM <tsnok777@gmail.com>
2022-12-08 16:43:09 +0200

    home page output screen model cars

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import*
+from django.utils.html import format_html
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40" style="border-radius: 10px;"/>'.format(object.car_photo.url))
+
+    thumbnail.short_description = 'Photo'
+
+    list_display = ('car_title', 'thumbnail', 'state', 'vin_no','is_featured')
+    list_display_links = ('car_title', 'thumbnail', 'state', 'vin_no')
+    search_fields = ('car_title', 'state', 'vin_no')
+    list_filter = ('car_title', 'state', 'vin_no')
+    list_editable = ('is_featured',)
+
 
 # Register your models here.
+admin.site.register(Car,CarAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
+from cars.models import *
 
 # Create your views here.
 def home(request):
     team = Team.objects.all()
+    featured_cars = Car.objects.order_by("-created_date").filter(is_featured=True)
+    all_cars = Car.objects.order_by("created_date")
     data = {
         'teams':team,
+        'featured_cars':featured_cars,
+        'all_cars':all_cars,
     }
     return render(request, 'pages/home.html',data)
 def about(request):
