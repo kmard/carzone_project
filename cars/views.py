d1870c78b3d5e5db4fc9ad921b5cfb65ab85e619
KM <tsnok777@gmail.com>
2022-12-21 15:15:50 +0200

    Setup Search Form & Search Functionality

--- cars/views.py
+++ cars/views.py
@@ -24,7 +24,27 @@ def search(request):
         keyword = request.GET.get('keyword')
         if keyword:
             cars = cars.filter(description__icontains = keyword)
-
+    if 'model' in request.GET:
+        model = request.GET.get('model')
+        if model:
+            cars = cars.filter(model__iexact = model)
+    if 'city' in request.GET:
+        city = request.GET.get('city')
+        if city:
+            cars = cars.filter(city__iexact = city)
+    if 'year' in request.GET:
+        year = request.GET.get('year')
+        if year:
+            cars = cars.filter(year__iexact = year)
+    if 'body_style' in request.GET:
+        body_style = request.GET.get('body_style')
+        if body_style:
+            cars = cars.filter(body_style__iexact = body_style)
+    if 'min_price' in request.GET:
+        min_price = request.GET.get('min_price')
+        max_price = request.GET.get('max_price')
+        if max_price:
+            cars = cars.filter(price__gte = min_price,price__lte = max_price)
     data = {
        'cars':cars,
     }
