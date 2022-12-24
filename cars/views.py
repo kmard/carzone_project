ffcf19c64015a2ca7abeb939bfa92406fddf6da6
KM <tsnok777@gmail.com>
2022-12-24 13:49:10 +0200

    Finishing Up All Search Form Functionalities

--- cars/views.py
+++ cars/views.py
@@ -9,8 +9,18 @@ def cars(request):
     paginator = Paginator(cars,2)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
+
+    model_search = Car.objects.values_list('model',flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     data = {
         'cars':paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
     }
     return render(request, 'cars/cars.html',data)
 
@@ -20,6 +30,13 @@ def car_detail(request,id):
 
 def search(request):
     cars = Car.objects.order_by('-created_date')
+
+    model_search = Car.objects.values_list('model',flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    transmission_search = Car.objects.values_list('transmission', flat=True).distinct()
+
     if 'keyword' in request.GET:
         keyword = request.GET.get('keyword')
         if keyword:
@@ -46,6 +63,11 @@ def search(request):
         if max_price:
             cars = cars.filter(price__gte = min_price,price__lte = max_price)
     data = {
-       'cars':cars,
+        'cars': cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
+        'transmission_search': transmission_search,
     }
     return render(request, 'cars/search.html',data)
