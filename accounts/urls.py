9182209d86e29529b08d4ea9c1d2eecd44afd70e
KM <tsnok777@gmail.com>
2022-12-27 13:36:48 +0200

    create app accounts and Login, Register & Dashboard Templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cars,name='cars'),
-    path('<int:id>', views.car_detail,name='car_detail'),
-    path('search', views.search,name='search'),
+    path('login', views.login,name='login'),
+    path('register', views.register,name='register'),
+    path('logout', views.logout,name='logout'),
+    path('dashboard', views.dashboard,name='dashboard'),
+
 ]
--- carzone/settings.py
+++ carzone/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'cars',
+    'accounts',
     'ckeditor',
     'django.contrib.humanize',
 ]
