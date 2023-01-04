ca7551f2fd4730a8062b617899bccb5a4c41fdd2
KM <tsnok777@gmail.com>
2023-01-04 17:39:55 +0200

    Send Emails,  Dashboard Functionality

--- carzone/settings.py
+++ carzone/settings.py
@@ -158,3 +158,10 @@ MESSAGE_TAGS = {
 }
 
 SITE_ID = 1
+
+#Email sending
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'test@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,11 @@
 from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+
+from carzone import settings
 from contacts.models import Contact
+from django.core.mail import send_mail
+
 
 def inquiry(request):
     if request.method == 'POST':
@@ -16,11 +21,27 @@ def inquiry(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
 
-        contact = Contact(car_id=car_id, user_id=user_id,first_name =first_name,
-                          last_name = last_name,customer_need = customer_need,
-                          city = city,state = state,email = email,phone = phone,
-                          message = message,car_title = car_title)
-        contact.save()
-        messages.success(request,'Your request has been submitted, we will back to you soon')
-        return redirect('/cars/'+car_id)
+        if request.user.is_authenticated:
+            user_id = request.POST.get('user_id')
+            has_contacted = Contact.objects.all().filter(car_id=car_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry about this car. Please wait answer.')
+                return redirect('/cars/' + car_id)
 
+        contact = Contact(car_id=car_id, user_id=user_id, first_name=first_name,
+                          last_name=last_name, customer_need=customer_need,
+                          city=city, state=state, email=email, phone=phone,
+                          message=message, car_title=car_title)
+
+        # admin_info = User.objects.get(is_superuser=True).email
+        # send_mail(
+        #     'New Car Inquiry',
+        #     message,
+        #     settings.EMAIL_HOST_USER,
+        #     ['to@example.com',admin_info],
+        #     fail_silently=False,
+        # )
+
+        contact.save()
+        messages.success(request, 'Your request has been submitted, we will back to you soon')
+        return redirect('/cars/' + car_id)
